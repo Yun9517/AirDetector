cd1d2649ddcdbbda0e88183748ea936141fefb4a
Raymond <raymond_yang@microjet.com.tw>
2017-11-23 16:36:25 +0800

    UI初步規劃

---
+++ app/src/main/java/microjet/com/airqi2/Fragment/MainFragment.kt
@@ -0,0 +1,47 @@
+package microjet.com.airqi2.Fragment
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import microjet.com.airqi2.CustomAPI.ColorArcProgressBar
+import microjet.com.airqi2.R
+
+/**
+ * Created by ray650128 on 2017/11/23.
+ */
+
+class MainFragment : Fragment() {
+
+    private var mContext : Context? = null
+
+    private var bar1 : ColorArcProgressBar? = null
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+
+        mContext = this.context.applicationContext
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater!!.inflate(R.layout.frg_main, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        bar1 = this.view!!.findViewById(R.id.tvocBar)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        bar1!!.setCurrentValues(10f)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+    }
+}
--- app/src/main/java/microjet/com/airqi2/MainActivity.kt
+++ app/src/main/java/microjet/com/airqi2/MainActivity.kt
@@ -1,13 +1,71 @@
 package microjet.com.airqi2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import microjet.com.airqi2.CustomAPI.FragmentAdapter
+import microjet.com.airqi2.Fragment.MainFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mContext = this@MainActivity
+
+    private val mFragmentList = ArrayList<Fragment>()
+
+    // ViewPager
+    private var mPageVp: ViewPager? = null
+
+    // ViewPager目前頁面
+    private var currentIndex: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        uiFindViewById()
+        viewPagerInit()
+    }
+
+    private fun uiFindViewById() {
+        mPageVp = this.findViewById(R.id.id_page_vp)
+    }
+
+    private fun viewPagerInit() {
+        // 加入 Fragment 成員
+        val mMainFg = MainFragment()
+
+        mFragmentList.add(mMainFg)
+
+        val mFragmentAdapter = FragmentAdapter(
+                this.supportFragmentManager, mFragmentList)
+        mPageVp!!.adapter = mFragmentAdapter
+        mPageVp!!.currentItem = 0
+
+        mPageVp!!.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            // state：滑動狀態（0，1，2）
+            // 1：滑動中 2：滑動完畢 0：閒置。
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            /**************************
+             * position :目前頁面，以及你點擊滑動的頁面 offset:目前頁面偏移的百分比
+             * offsetPixels:目前頁面偏移的像素位置
+             */
+            override fun onPageScrolled(position: Int, offset: Float,
+                                        offsetPixels: Int) {
+
+                Log.e("offset:", offset.toString() + "")
+            }
+
+            override fun onPageSelected(position: Int) {
+                currentIndex = position
+            }
+        })
 
     }
 }
