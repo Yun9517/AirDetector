fd8f40c7ee17b0cce24ccb49addabf1fffdf7ec4
AndyYeh <andy00307andr0id@gmail.com>
2017-11-28 18:54:29 +0800

    新增(創資料庫+新增資料庫內容+查詢資料庫內容)

---
+++ app/src/main/java/microjet/com/airqi2/AndyAirDBhelper.java
@@ -0,0 +1,71 @@
+package microjet.com.airqi2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AndyＡirDBhelper extends SQLiteOpenHelper {
+	//final static String database = "AirDataBase";
+	//final static String database2 = "AirDataBase2";
+	//final static String database3 = "AirDataBase3";
+	//final static String database4 = "AirDataBase4";
+    //final static String database5 = "AirDataBase5";
+	//final static String database6 = "AirDataBase6";
+	//final static String database7 = "AirDataBase7";
+	//final static String database8 = "AirDataBase8";
+	//final static String database9 = "AirDataBase9";
+	//final static String database10 = "AirDataBase10";
+	//final static String database11 = "AirDataBase11";
+	//final static String database12 = "AirDataBase12";
+	//final static String database13 = "AirDataBase13";
+    //final static String database14 = "AirDataBase14";
+    final static String database15 = "AirDataBase15";
+	//final static int version = 1;
+	//final static int version2 = 3;
+	//final static int version3 = 3;
+	//final static int version4 = 3;
+    //final static int version5 = 3;
+	//final static int version6 = 3;
+	//final static int version7 = 3;
+	//final static int version8 = 3;
+	//final static int version9 = 3;
+	//final static int version10 = 3;
+	//final static int version11 = 3;
+	//final static int version12 = 3;
+	//final static int version13 = 3;
+    //final static int version14 = 3;
+    final static int version15 = 4;
+
+
+    public static SQLiteDatabase dbrw;
+
+	// 內建的建構子，用來建立資料庫
+	public AndyＡirDBhelper(Context context, String name, CursorFactory factory,
+                           int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	// 自建的建構子，只需傳入一個Context物件即可
+	public AndyＡirDBhelper(Context context) {
+		super(context, database15, null, version15);
+	}
+	//建立資料表
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+        db.execSQL(String.format("CREATE TABLE airtable(_id integer primary key autoincrement,temper text no null,hum  text no null,tvoc  text no null,CO2  text no null)"));
+	}
+
+	//資料庫更新，刪除資料表，再次呼叫onCreate()重建資料表
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldversion, int newversion) {
+		if(newversion>oldversion){
+			// TODO Auto-generated method stub
+        	db.execSQL("DROP TABLE IF EXISTS airtable");
+			onCreate(db);
+		}
+	}
+
+}
