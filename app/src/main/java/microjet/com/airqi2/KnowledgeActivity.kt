9a06b21e6f8ee84f7fac824b175d9c44c0933b4a
Raymond <raymond_yang@microjet.com.tw>
2017-11-29 15:45:44 +0800

    知識庫增加簡中＆英文

--- app/src/main/java/microjet/com/airqi2/KnowledgeActivity.kt
+++ app/src/main/java/microjet/com/airqi2/KnowledgeActivity.kt
@@ -1,19 +1,26 @@
 package microjet.com.airqi2
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.webkit.WebView
+import java.util.*
 
 class KnowledgeActivity : AppCompatActivity() {
 
     private var webView: WebView? = null
+
+    private var mContext: Context? = null
 
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_knowledge)
 
         webView = findViewById(R.id.webView)
+
+        mContext = this@KnowledgeActivity.applicationContext
 
         readHtmlFormAssets()
     }
@@ -25,7 +32,18 @@ class KnowledgeActivity : AppCompatActivity() {
         webSettings.loadWithOverviewMode = true
         webSettings.useWideViewPort = true
 
+        val mLang = Locale.getDefault().language + "-" + Locale.getDefault().country
+
+        Log.v("KnowledgeActivity", "Current language is: $mLang")
+
         webView!!.setBackgroundColor(Color.TRANSPARENT)  //  WebView 背景透明效果，不知道为什么在xml配置中无法设置？
-        webView!!.loadUrl("file:///android_asset/knowledge/index.html")
+
+        if(mLang == "zh-TW" || mLang == "zh-HK") {
+            webView!!.loadUrl("file:///android_asset/knowledge/index.html")
+        } else if (mLang == "zh-CN"){
+            webView!!.loadUrl("file:///android_asset/knowledge/index-zh_CN.html")
+        } else {
+            webView!!.loadUrl("file:///android_asset/knowledge/index-en_US.html")
+        }
     }
 }
