2d2a528edceacca51d5d8a947f1d764b96e03813
Richard <starengram@gmail.com>
2017-12-08 15:31:19 +0800

    V1.0.6 將progress bar的值改回TVOC  D

--- app/src/main/java/microjet/com/airqi2/MainActivity.kt
+++ app/src/main/java/microjet/com/airqi2/MainActivity.kt
@@ -933,7 +933,7 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
                     stringArray.add(intent.getStringExtra("CO2Value"))
                     stringArray.add(intent.getStringExtra("BatteryLife"))
                     //(mFragmentAdapter.getItem(0)as MainFragment).setCurrentValue(stringArray)
-                    (mFragmentAdapter.getItem(0)as MainFragment).setBar1CurrentValue(intent.getStringExtra("TemperatureValue"))
+                    (mFragmentAdapter.getItem(0)as MainFragment).setBar1CurrentValue(intent.getStringExtra("TVOCValue"))
                     //val mFragmentAdapter :FragmentAdapter=mPageVp?.adapter as FragmentAdapter
                     (mFragmentAdapter.getItem(1)as TVOCFragment).setRealTimeBarData(intent.getStringExtra("TVOCValue"),intent.getStringExtra("BatteryLife"))
                     //(mFragmentAdapter.getItem(1)as TVOCFragment).AddedSQLlite(data)
