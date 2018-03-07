c69da7945e05344580693900ac515ab38a021829
Petercchsieh5157@gmail.com <cchsieh5157@gmail.com>
2018-03-07 16:09:21 +0800

    加英文

--- app/src/main/java/com/microjet/airqi2/Fragment/ChartFragment.kt
+++ app/src/main/java/com/microjet/airqi2/Fragment/ChartFragment.kt
@@ -353,7 +353,7 @@ class ChartFragment: Fragment() {
                     DEFINE_FRAGMENT_TVOC -> {
                         val temp = e?.`val`
                         if (temp == 65538f) {
-                            mTextViewValue!!.text = "沒有偵測"
+                            mTextViewValue!!.text = getString(R.string.not_yetDetected)
                         } else {
                             mTextViewValue!!.text = temp?.toInt().toString() + " ppb"
                         }
@@ -363,7 +363,7 @@ class ChartFragment: Fragment() {
                     DEFINE_FRAGMENT_ECO2 -> {
                         val temp = e?.`val`
                         if (temp == 65538f) {
-                            mTextViewValue!!.text = "沒有偵測"
+                            mTextViewValue!!.text = getString(R.string.not_yetDetected)
                         } else {
                             mTextViewValue!!.text = temp?.toInt().toString() + " ppm"
                         }
@@ -374,7 +374,7 @@ class ChartFragment: Fragment() {
                         val temp: Float? = e?.`val`
                         val temp1: Float? = (temp!! - 10.0f)
                         if (temp1!! == 65528f) {
-                            mTextViewValue!!.text = "---" + " ℃"
+                            mTextViewValue!!.text = getString(R.string.not_yetDetected)
                         } else {
                             val newTemp = "%.1f".format(temp1)
                             mTextViewValue!!.text = "$newTemp ℃"
@@ -385,7 +385,7 @@ class ChartFragment: Fragment() {
                     DEFINE_FRAGMENT_HUMIDITY -> {
                         val temp = e?.`val`
                         if (temp == 65538f) {
-                            mTextViewValue!!.text = "沒有偵測"
+                            mTextViewValue!!.text = getString(R.string.not_yetDetected)
                         } else {
                             mTextViewValue!!.text = temp?.toInt().toString() + " %"
                         }
