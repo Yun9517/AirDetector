720c67f3d7c146878008e2e6d2927841799e2f32
Raymond <raymond_yang@microjet.com.tw>
2018-03-12 18:00:48 +0800

    空氣地圖初步實現描點，取今天紀錄的最後一筆顯示在地圖上

--- app/src/main/java/com/microjet/airqi2/AirMapActivity.kt
+++ app/src/main/java/com/microjet/airqi2/AirMapActivity.kt
@@ -2,10 +2,7 @@ package com.microjet.airqi2
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Criteria
-import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat.checkSelfPermission
 import android.support.v4.app.ActivityCompat.requestPermissions
@@ -19,7 +16,12 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.maps.model.MarkerOptions
+import io.realm.Realm
+import io.realm.Sort
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -29,7 +31,131 @@ import com.google.android.gms.tasks.OnCompleteListener
 
 class AirMapActivity: AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var mMap: GoogleMap
 
+    private val REQUEST_LOCATION = 2
+    private val perms: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_airmap)
+
+        initActionBar()
+        initGoogleMapFragment()
+
+        createLocationRequest()
+    }
+
+    // 資料庫查詢
+    @SuppressLint("SimpleDateFormat")
+    private fun realmDataQuery() {
+        val realm = Realm.getDefaultInstance()
+        val query = realm.where(AsmDataModel::class.java)
+
+        val calendar = Calendar.getInstance()
+        /*calendar.set(Calendar.HOUR, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        val startDate = (calendar.timeInMillis + calendar.timeZone.rawOffset) / 10 // 獲取當前時間
+
+        calendar.set(Calendar.HOUR, 23)
+        calendar.set(Calendar.MINUTE, 59)
+        calendar.set(Calendar.SECOND, 59)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        val endDate = (calendar.timeInMillis + calendar.timeZone.rawOffset) / 10 // 獲取當前時間
+
+        val result = query.between("Created_time", startDate, endDate).findAll()//query.findAll()//
+
+        Log.i("DATE", "Database between: $startDate, $endDate. Result size: ${result.size}")*/
+
+        //現在時間實體毫秒
+        val touchTime = calendar.timeInMillis + calendar.timeZone.rawOffset
+        //將日期設為今天日子加一天減1秒
+        val startTime = touchTime / (3600000 * 24) * (3600000 * 24)
+        val endTime = startTime + TimeUnit.DAYS.toMillis(1) - TimeUnit.SECONDS.toMillis(1)
+        query.between("Created_time", startTime, endTime).sort("Created_time", Sort.ASCENDING)
+        val result = query.findAll()
+        Log.d("DATE", "Today total count: ${result.size}")
+
+        mMap.addMarker(MarkerOptions()
+                .position(LatLng((result[result.size - 1]!!.latitude).toDouble(),
+                        (result[result.size - 1]!!.longitude).toDouble()))
+                .title("TVOC: ${result[result.size - 1]!!.tvocValue}")
+        )
+    }
+
+    // 初始化ActionBar
+    private fun initActionBar() {
+        // 取得 actionBar
+        val actionBar = supportActionBar
+        // 設定顯示左上角的按鈕
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    // 設定ActionBar返回鍵的動作
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home //對用戶按home icon的處理，本例只需關閉activity，就可返回上一activity，即主activity。
+            -> {
+                finish()
+                return true
+            }
+            else -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // 初始化GoogleMap UI元件
+    private fun initGoogleMapFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    // 初始化位置，由於已經先在onMapReady()中要求權限了，因此無需再次要求權限
+    @SuppressLint("MissingPermission")
+    private fun initLocation() {
+        val client = LocationServices.getFusedLocationProviderClient(this)
+
+        client.lastLocation.addOnCompleteListener(this, {
+            if(it.isSuccessful) {
+                val location = it.result
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        LatLng(location.latitude, location.longitude), 15f))
+            }
+
+            Log.i("LOCATION", "Location Task is Successful: ${it.isSuccessful}")
+        })
+    }
+
+    // 設定位置要求的參數
+    private fun createLocationRequest() {
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 2000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    // 權限要求結果，由於已經先在onMapReady()中要求權限了，因此在處理的程式碼中無需再次要求權限
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        when(requestCode) {
+            REQUEST_LOCATION -> {
+                mMap.isMyLocationEnabled =
+                        grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+                initLocation()
+            }
+        }
+    }
+
+    // 當 Map 可用時做相關處理
     override fun onMapReady(p0: GoogleMap?) {
         mMap = p0!!
 
@@ -47,83 +173,8 @@ class AirMapActivity: AppCompatActivity(), OnMapReadyCallback {
             mMap.uiSettings.isZoomControlsEnabled = true
 
             initLocation()
-        }
-    }
 
-    private lateinit var mMap: GoogleMap
-
-    private val REQUEST_LOCATION = 2
-    private val perms: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_airmap)
-
-        initActionBar()
-        initGoogleMapFragment()
-
-        createLocationRequest()
-    }
-
-    private fun initActionBar() {
-        // 取得 actionBar
-        val actionBar = supportActionBar
-        // 設定顯示左上角的按鈕
-        actionBar!!.setDisplayHomeAsUpEnabled(true)
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home //對用戶按home icon的處理，本例只需關閉activity，就可返回上一activity，即主activity。
-            -> {
-                finish()
-                return true
-            }
-            else -> {
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun initGoogleMapFragment() {
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun initLocation() {
-        val client = LocationServices.getFusedLocationProviderClient(this)
-
-        client.lastLocation.addOnCompleteListener(this, {
-            if(it.isSuccessful) {
-                val location = it.result
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                        LatLng(location.latitude, location.longitude), 15f))
-            }
-
-            Log.i("LOCATION", "Location Task is Successful: ${it.isSuccessful}")
-        })
-    }
-
-    private fun createLocationRequest() {
-        val locationRequest = LocationRequest()
-        locationRequest.interval = 5000
-        locationRequest.fastestInterval = 2000
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<out String>,
-                                            grantResults: IntArray) {
-        when(requestCode) {
-            REQUEST_LOCATION -> {
-                mMap.isMyLocationEnabled =
-                        grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
-
-                initLocation()
-            }
+            realmDataQuery()
         }
     }
 }
