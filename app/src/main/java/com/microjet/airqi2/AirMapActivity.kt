fdd14eda29fa3be355328b27098b986cfb8da602
Raymond <raymond_yang@microjet.com.tw>
2018-03-12 13:16:18 +0800

    MainActivity改singleTask看看會不會又被重新建立一個新實體&調整空氣地圖中

--- app/src/main/java/com/microjet/airqi2/AirMapActivity.kt
+++ app/src/main/java/com/microjet/airqi2/AirMapActivity.kt
@@ -1,61 +1,60 @@
 package com.microjet.airqi2
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
+import android.location.Criteria
+import android.location.LocationManager
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import android.support.v4.app.ActivityCompat.checkSelfPermission
+import android.support.v4.app.ActivityCompat.requestPermissions
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.util.Log
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import android.location.LocationManager
-import android.location.Criteria
-
-
-
-
 
 
 /**
  * Created by B00174 on 2017/11/27.
+ *
  */
 
-class AirMapActivity : AppCompatActivity(), GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener,
-        OnMapReadyCallback,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+class AirMapActivity: AppCompatActivity(), OnMapReadyCallback {
 
+
+    override fun onMapReady(p0: GoogleMap?) {
+        mMap = p0!!
+
+        /*val howBonBon = LatLng(25.029639, 121.544416)
+        mMap.addMarker(MarkerOptions()
+                .position(howBonBon)
+                .title("好棒棒！"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(howBonBon))*/
+
+        if(checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(this, perms, REQUEST_LOCATION)
+        } else {
+            mMap.isMyLocationEnabled = true
+        }
+    }
+
+    private lateinit var mLocationManager: LocationManager
     private lateinit var mMap: GoogleMap
 
-    /**
-     * Request code for location permission request.
-     *
-     * @see .onRequestPermissionsResult
-     */
-    private val LOCATION_PERMISSION_REQUEST_CODE = 1
-
-    /**
-     * Flag indicating whether a requested permission has been denied after returning in
-     * [.onRequestPermissionsResult].
-     */
-    private var mPermissionDenied = false
+    private val REQUEST_LOCATION = 2
+    private val perms: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_airmap)
 
         initActionBar()
-
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        initGoogleMapFragment()
     }
 
     private fun initActionBar() {
@@ -65,94 +64,43 @@ class AirMapActivity : AppCompatActivity(), GoogleMap.OnMyLocationButtonClickLis
         actionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-
-        mMap = googleMap
-
-        mMap.setOnMyLocationButtonClickListener(this)
-        mMap.setOnMyLocationClickListener(this)
-
-
-        // Add a marker in Sydney and move the camera
-        //val sydney = LatLng(-34.0, 151.0)
-        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Taipei"))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        enableMyLocation()
+    private fun initGoogleMapFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Enables the My Location layer if the fine location permission has been granted.
-     */
-    private fun enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Permission to access the location is missing.
-            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
-                    Manifest.permission.ACCESS_FINE_LOCATION, true)
-        } else if (mMap != null) {
-            // Access to the location has been granted to the app.
-            mMap.isMyLocationEnabled = true
+    @SuppressLint("MissingPermission")
+    private fun initLocation() {
+        mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-            val service = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            val criteria = Criteria()
-            val provider = service.getBestProvider(criteria, false)
-            val location = service.getLastKnownLocation(provider)
-            val userLocation = LatLng(location.latitude, location.longitude)
+        val criteria = Criteria()
+        criteria.accuracy = Criteria.ACCURACY_FINE
 
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
+        val provider = mLocationManager.getBestProvider(criteria, true)
+
+        val location = mLocationManager.getLastKnownLocation(provider)
+
+        if(location != null) {
+            Log.i("LOCATION", "${location.latitude}, ${location.longitude}")
+
+            mMap.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                            LatLng(location.latitude, location.longitude), 15f)
+            )
         }
     }
 
-    override fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false
-    }
-
-    override fun onMyLocationClick(location: Location) {
-        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
                                             grantResults: IntArray) {
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-            return
-        }
+        when(requestCode) {
+            REQUEST_LOCATION -> {
+                mMap.isMyLocationEnabled =
+                        grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
 
-        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // Enable the my location layer if the permission has been granted.
-            enableMyLocation()
-        } else {
-            // Display the missing permission error dialog when the fragments resume.
-            mPermissionDenied = true
+                initLocation()
+            }
         }
-    }
-
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-        if (mPermissionDenied) {
-            // Permission was not granted, display error dialog.
-            showMissingPermissionError()
-            mPermissionDenied = false
-        }
-    }
-
-    /**
-     * Displays a dialog with error message explaining that the location permission is missing.
-     */
-    private fun showMissingPermissionError() {
-        PermissionUtils.PermissionDeniedDialog
-                .newInstance(true).show(supportFragmentManager, "dialog")
     }
 }
