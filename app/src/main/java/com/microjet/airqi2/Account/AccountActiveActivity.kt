a3dbfe36cc5672ae72e83bfa796587d6a92b041c
Petercchsieh5157@gmail.com <cchsieh5157@gmail.com>
2018-03-19 11:42:59 +0800

    Test send text to LINE

--- app/src/main/java/com/microjet/airqi2/Account/AccountActiveActivity.kt
+++ app/src/main/java/com/microjet/airqi2/Account/AccountActiveActivity.kt
@@ -2,6 +2,7 @@ package com.microjet.airqi2.Account
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -83,6 +84,13 @@ class AccountActiveActivity : AppCompatActivity() {
             //finish()
         }
         //DownloadTask().execute()
+
+        // 03/19 Share to Line
+        shareData.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("line://msg/text" + "123321"))
+        }
+
     }
     private fun initActionBar() {
         // 取得 actionBar
