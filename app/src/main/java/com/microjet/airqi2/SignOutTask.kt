2642551064f50484d4021cfc0b364aa318fe0f0c
Peter <yun_hsieh@microjet.com.tw>
2018-06-13 14:45:12 +0800

    用最新Develope重做昨日的進度

---
+++ app/src/main/java/com/microjet/airqi2/SignOutTask.kt
@@ -0,0 +1,44 @@
+package com.microjet.airqi2
+
+import android.content.ContentValues.TAG
+import android.os.AsyncTask
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import okhttp3.*
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+class SignOutTask: AsyncTask<String, Int, String>() {
+
+    override fun doInBackground(vararg params: String): String? {
+        try{
+            val client = OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build()
+            val mediaType = MediaType.parse("application/x-www-form-urlencoded")
+            val body: RequestBody = RequestBody.create(mediaType, "registration_id=" + FirebaseInstanceId.getInstance().token)
+            Log.e("par", params.first())
+            val request = Request.Builder()
+                    .url("https://mjairql.com/api/v1/logout")
+                    .post(body)
+                    .addHeader("authorization",params.first())//get token
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .addHeader("Cache-Control", "no-cache")
+                    .build()
+            val response = client.newCall(request).execute()
+            Log.e("backLogOut", response.body()?.string().toString())
+
+            if (!response.isSuccessful) { Log.d(TAG, "ERROR") }
+            else {
+
+            }
+        } catch (e: Exception) {
+            Log.e("backLogOutE", "restError")
+            Log.e(TAG, e.toString())
+        }
+        return null
+    }
+
+}
