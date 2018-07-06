cab65df7e2b57200aedfcdb9ac41fd385a8d8819
B00055 <chungyenlai@gmail.com>
2018-07-06 10:47:27 +0800

    修正預設值為"-----"時轉換為float會出現格式錯誤的問題

--- app/src/main/java/com/microjet/airqi2/AirMapActivity.kt
+++ app/src/main/java/com/microjet/airqi2/AirMapActivity.kt
@@ -458,11 +458,7 @@ class AirMapActivity : AppCompatActivity(), OnMapReadyCallback, MJGraphView.MJGr
     @SuppressLint("SetTextI18n", "SimpleDateFormat")
     private fun updateValuePanel(timeVal: Long, tvocVal: String, pm25Val: String, eco2Val: String,
                                  tempVal: String, humiVal: String, latiVal: String, longiVal: String) {
-
-        val finalTempVal = Utils.convertTemperature(this@AirMapActivity, tempVal.toFloat())
-
         val dateFormat = SimpleDateFormat("HH:mm")
-
         textTIMEvalue.text = if(timeVal != 0L) {
             dateFormat.format(timeVal)
         } else {
@@ -476,9 +472,18 @@ class AirMapActivity : AppCompatActivity(), OnMapReadyCallback, MJGraphView.MJGr
         } else {
             "$pm25Val μg/m³"
         }
-
+        val myPref = PrefObjects(this)
+        val isFahrenheit = myPref.getSharePreferenceTempUnitFahrenheit()
         textECO2value.text = "$eco2Val ppm"
-        textTEMPvalue.text = finalTempVal
+        textTEMPvalue.text = if (tempVal=="-----") {
+            when (isFahrenheit){
+                true->{"$tempVal ℉"}
+                false->{"$tempVal ℃"}
+            }
+        }
+        else{
+            Utils.convertTemperature(this@AirMapActivity, tempVal.toFloat())
+        }
         textHUMIvalue.text = "$humiVal %"
         textLATIvalue.text = latiVal
         textLNGIvalue.text = longiVal
