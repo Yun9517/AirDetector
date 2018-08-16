8979ce4261ed50d6192a596d20f540d72c7082ab
HAO_DESTOP <hao_hsu@microjet.com.tw>
2018-08-16 16:15:57 +0800

    菜單上架，功能目前測過OK，菜單子項目在思考優化(´･ω･`)

--- app/src/main/java/com/microjet/airqi2/CustomAPI/ExpandedMenuModel.kt
+++ app/src/main/java/com/microjet/airqi2/CustomAPI/ExpandedMenuModel.kt
@@ -7,6 +7,7 @@ class ExpandedMenuModel {
 
     var iconName = ""
     var iconImg = -1 // menu icon resource id
+    var icHeadRidInt = 0
     var iconIndicator = 0
     var FWIndicator = 0
     var FWIndicatorChild = 0
